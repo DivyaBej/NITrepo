8d922a3404420dfcaa1982413371a4757e697928
lenovo <d1@gmail.com>
2021-06-30 12:30:41 +0530

    dev

--- gitDemo/src/com/gittest/test.java
+++ gitDemo/src/com/gittest/test.java
@@ -3,5 +3,9 @@ package com.gittest;
 import com.gitDemo.Utility;
 
 public class test {
-   
+   public static void main(String[] args) {
+	   Utility u = new Utility();
+	   System.out.println(u.getDate());
+	
+}
 }
