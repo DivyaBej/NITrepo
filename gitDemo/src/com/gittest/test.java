35ad8aaf9d326c9fc495702889aba5a3d60b671e
lenovo <d1@gmail.com>
2021-06-30 12:24:08 +0530

    tl del

--- gitDemo/src/com/gittest/test.java
+++ gitDemo/src/com/gittest/test.java
@@ -3,9 +3,5 @@ package com.gittest;
 import com.gitDemo.Utility;
 
 public class test {
-   public static void main(String[] args) {
-	   Utility u = new Utility();
-	   System.out.println(u.getDate());
-	
-}
+   
 }
